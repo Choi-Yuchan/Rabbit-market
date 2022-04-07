0d4e6b82ee5868815d4a6e6059af0d4f86bb8ecf
ChoiYuChan <cyc330@ontacthealth.com>
2022-04-07 15:03:48 +0900

    5.15 Add Stream

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16 ">
+    <div className="px-4 py-10 space-y-5 ">
       <div>
         <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md">
           <svg
@@ -23,7 +23,23 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="my-5 ">
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700"
           htmlFor="price"
@@ -48,18 +64,19 @@ const Upload: NextPage = () => {
       <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor=""
+          htmlFor="description"
         >
           Description
         </label>
 
         <textarea
+          id="description"
           className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
           rows={4}
         />
       </div>
-      <button className="mt-5 w-full bg-orange-500 text-gray-50 hover:bg-orange-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
-        Upload product
+      <button className="w-full bg-orange-500 text-gray-50 hover:bg-orange-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+        Upload item
       </button>
     </div>
   );

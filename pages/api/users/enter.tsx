27f055e9ca8a6df2ce608a834298327477cb4c97
ChoiYuChan <cyc330@ontacthealth.com>
2022-04-15 16:10:18 +0900

    #9.10 NextAuth

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,66 +1,58 @@
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
 
-  const payload = phone ? { phone: +phone } : { email };
-  const hash = Math.random().toString();
+  const user = phone ? { phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(Math.random() * 900000).toString();
   const token = await client.token.create({
     data: {
-      payload: hash,
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
-  console.log(token);
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) console.log("found it");
-  //   if (!user) {
-  //     console.log("Did not find. Will create.");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         email,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if (user) console.log("found it");
-  //   if (!user) {
-  //     console.log("Did not find. Will create.");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  return res.status(200).end();
+  if (phone) {
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `Your login token is ${payload}.`,
+    // });
+    // console.log(message);
+  } else if (email) {
+    // const email = await mail.send({
+    //   from: "cor330@naver.com",
+    //   to: "cor330@naver.com",
+    //   subject: "Your Rabbit-market Verification Email",
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
+  }
+
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
